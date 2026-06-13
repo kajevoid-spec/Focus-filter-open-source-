dac76d97bcad6b0019ab538c26d744662b02d6e9
kajevoid-spec <kajevoid@gmail.com>
2026-06-13 18:53:31 +0800

    Delete app directory

--- app/src/main/kotlin/com/focusfilter/data/db/AppUsageStat.kt
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2026 FocusFilter
-// SPDX-License-Identifier: GPL-3.0-or-later
-package com.focusfilter.data.db
-
-data class AppUsageStat(
-    val packageName: String,
-    val appName: String,
-    val count: Int
-)
